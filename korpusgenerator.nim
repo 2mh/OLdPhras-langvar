67a469ac42416a5fbb014603322467c9e58097d4
Simon Hafner <hafnersimon@gmail.com>
2011-12-16 20:33:26 +0100

    added multithreading

--- korpusgenerator.nim
+++ korpusgenerator.nim
@@ -1,6 +1,7 @@
 import strutils
 import tables
-import math
+import osproc
+import os
 
 iterator ngrams(korpus: string, n: int, word_based = false): tuple[history: seq[string], word: string] = 
   var
@@ -12,18 +13,15 @@ iterator ngrams(korpus: string, n: int, word_based = false): tuple[history: seq[
     history.delete(0)
     history.add(word)
 
-for korpus in ["1600_1650", "1650_1700", "1700_1750", "1750_1800", "1800_1850", "1850_1900", "1900_2010"].items():
+proc korpus_of(input_name: string, n: int): TTable[string, float] = 
   const
     join_char = "{"
-    n = 3
   var
     freqs = init_table[string, TCountTable[string]](128)
     hist: string
     sum: int
-    language_model = init_table[string, float]()
-    input_name = "t" & korpus & ".txt"
-    output_name = "k" & korpus
     
+  result = init_table[string, float]()
   for line in lines(input_name):
     for history, word in ngrams(line, n):
       hist = history.join(join_char) # join hack
@@ -34,7 +32,23 @@ for korpus in ["1600_1650", "1650_1700", "1700_1750", "1750_1800", "1800_1850", 
     sum = 0
     for number in counts.values: sum.inc(number)
     for word, count in counts.pairs:
-      language_model[history & join_char & word] = count/sum
-  var output_file = open(output_name, fmWrite)
-  for key, value in language_model.pairs:
-    output_file.write($value & " " & key & "\n")
+      result[history & join_char & word] = count/sum
+
+if os.paramCount() == 0:
+  var commands: seq[string] = @[]
+  for n in 1..6:
+    for korpus_name in ["1600_1650", "1650_1700", "1700_1750", "1750_1800", "1800_1850", "1850_1900", "1900_2010"].items():
+      commands.add(os.getAppFilename() & " " & korpus_name & " " & $n)
+  discard execProcesses(commands, n = 6)
+
+else:
+  var
+    korpus_name = os.paramStr(1)
+    n = os.paramStr(2).parse_int
+    input_name = "t" & korpus_name & ".txt"
+    output_name = "k" & korpus_name & "W" & $n
+    korpus = korpus_of(input_name, n)
+    output_file = open(output_name, fmWrite)
+  echo output_name
+  for key, value in korpus.pairs:
+    output_file.write(formatFloat(value) & " " & key & "\n")
