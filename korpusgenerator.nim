9cae5e2ac332db56e8a979d5246fa35d3dfbcc12
Simon Hafner <hafnersimon@gmail.com>
2011-12-16 11:09:28 +0100

    fixes

--- korpusgenerator.nim
+++ korpusgenerator.nim
@@ -15,16 +15,16 @@ iterator ngrams(korpus: string, n: int, word_based = false): tuple[history: seq[
 for korpus in ["1600_1650", "1650_1700", "1700_1750", "1750_1800", "1800_1850", "1850_1900", "1900_2010"].items():
   const
     join_char = "{"
+    n = 3
   var
     freqs = init_table[string, TCountTable[string]](128)
-    n = 3
     hist: string
     sum: int
     language_model = init_table[string, float]()
-    input_file = "t" & korpus & ".txt"
-    output_file = "k" & korpus
+    input_name = "t" & korpus & ".txt"
+    output_name = "k" & korpus
     
-  for line in lines(input_file):
+  for line in lines(input_name):
     for history, word in ngrams(line, n):
       hist = history.join(join_char) # join hack
       if not freqs.has_key(hist):
@@ -35,5 +35,6 @@ for korpus in ["1600_1650", "1650_1700", "1700_1750", "1750_1800", "1800_1850", 
     for number in counts.values: sum.inc(number)
     for word, count in counts.pairs:
       language_model[history & join_char & word] = count/sum
+  var output_file = open(output_name, fmWrite)
   for key, value in language_model.pairs:
-    output_file.write(value & " " & key & "\n")
+    output_file.write($value & " " & key & "\n")
