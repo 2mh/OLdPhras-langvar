350d96d39ffdcfad803eef9bfcc5b691190f4a13
Reto <reto@reto-acer.(none)>
2012-01-18 16:50:44 +0100

    Splitter fuer Kreuzvalidierung

---
+++ korpus_splitter.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+# NLP für historische Dokumente, HS2011
+# Reto Baumgartner
+
+import sys
+import glob, os
+
+# Ordner mit Quelldateien
+ROOT = "./"
+
+# Anzahl der Korpora für Kreuzvalidierung
+corpora = 20
+
+# Zähler der Sätze
+counter = 0
+
+# Schreibt die Zeile in das Testkorpus, wenn es das x-te ist,
+# sonst ins Trainingskorpus
+def writeLine(line, test, train):
+    global counter
+    for x in range(corpora):
+        if counter % corpora == x:
+            test[x].write(line)
+        else:
+            train[x].write(line)
+    counter += 1
+    
+# Verarbeitet eine Epochendatei
+def processFile(filename):
+    epoch = filename.split(".")[2]
+    thisfile = open(filename, "r")
+    lines = thisfile.readlines()
+    thisfile.close()
+    test = []
+    train = []
+    for x in range(corpora):
+        test.append(open("test_" + str(x) + "." + epoch, "w"))
+        train.append(open("train_" + str(x) + "." + epoch, "w"))
+    for line in lines:
+        writeLine(line, test, train)
+    for x in range(corpora):
+        test[x].close()
+        train[x].close()
+
+for filename in glob.glob("%s/rawcorp.*" % ROOT):
+    print(filename)
+    processFile(filename)
+    
+#print("Anzahl Sätze: " + str(counter))
