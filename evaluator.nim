b9a78def824aae2f6fc2d9545acc266f5e73110d
Simon Hafner <hafnersimon@gmail.com>
2012-01-20 22:45:29 +0100

    more parallelize

--- evaluator.nim
+++ evaluator.nim
@@ -16,24 +16,29 @@ const
   tests = 0
   orders = 6
   charBased = [0, 1]
-  resultFileName = "results_test_$#_char_$#_order_$#"
+  resultFileName = "results_test_$#_char_$#_order_$#_tested_$#"
   perLineOutput = "actual $# result $# $#"
   guessOutput = "guess actual $# result $# found $# times if above $#"
   resultOutput = "correct testkorpus $# $# if above $#"
 
-template korpus_name(): expr = ($korpus).substr(1)
-template korpus_file(): expr = "train_" & $test & "." & korpus_name
+template korpusname(): expr = ($korpus).substr(1)
+template korpusfile(): expr = "train_" & $test & "." & korpusname
 
 if os.paramCount() == 0:
-  var prepare: seq[string] = @[]
-  var commands: seq[string] = @[]
+  # generation
+  var
+    prepare: seq[string] = @[]
+    commands: seq[string] = @[]
   for test in 0..tests:
     for based in charBased:
-      prepare.add(os.getAppFilename() & " " & $test & " " & $based & " " & $1)
-      for order in 2..orders:
-        commands.add(os.getAppFilename() & " " & $test & " " & $based & " " & $order)
-  if execProcesses(prepare, n = 6) != 0:
-    raise newException(EIO, "preparation failed, aborting!")
+      for order in 1..orders:
+        for korpus in low(TKorpus)..high(TKorpus):
+          prepare.add("./korpusgenerator" & " " & korpusfile & " " & $based.ord & " " & $order)
+          commands.add(os.getAppFilename() & " " & $test & " " & $based & " " & $order & " " & $korpus)
+  if execProcesses(prepare) != 0:
+    #raise newException(EIO, "corpi not written")
+  else:
+    echo "korpi generated!"
   discard execProcesses(commands, n = 6)
 
 else:
@@ -41,17 +46,13 @@ else:
     test = os.paramStr(1).parseInt
     charBased = bool(os.paramStr(2).parseInt)
     order = os.paramStr(3).parseInt
+    s = os.paramStr(4)
     korpi: array[TKorpus, PLanguageModel]
+    totest: TKorpus
+  for k in low(TKorpus)..high(TKorpus):
+    if $k == s:
+      totest = k
 
-  var commands: seq[string] = @[]
-  # generation
-  for korpus in low(TKorpus)..high(TKorpus):
-    echo "./korpusgenerator" & " " & korpusfile & " " & $charBased.ord & " " & $order
-    commands.add("./korpusgenerator" & " " & korpusfile & " " & $charBased.ord & " " & $order)
-  if execProcesses(commands) != 0:
-    raise newException(EIO, "corpi not written")
-  else:
-    echo "korpi generated!"
   for korpus in low(TKorpus)..high(TKorpus):
     var
       inputFile = open("trained_" & korpus_file & $charBased & $order)
@@ -62,25 +63,27 @@ else:
       lm.fallback = load(fallbackFile)
     korpi[korpus] = lm
     
-  var resultFile = open(resultFileName % [$test, $CharBased.ord, $order], fmWrite)
-  for korpus in low(TKorpus)..high(TKorpus):
-    var
-      runs: seq[TRun] = @[]
-    for line in lines("test_" & $test & "." & korpus_name):
-      if line.len > 20:
-        var run: TRun
-        # run the tests
-        run.result = recognize[TKorpus](korpi, line)
-        run.length = len(line)
-        runs.add(run)
-    # accumulate the results
-    for above in [20,50,100,150]:
-      var accumulatedResults: array[TKorpus, int]
-      for i,j in accumulatedResults: accumulatedResults[i] = 0 # clear the array
-      for run in runs:
-        if run.length < above: continue
-        inc(accumulatedResults[run.result])
-      for guess, count in accumulatedResults:
-        resultFile.writeln(guessOutput % [$korpus, $guess, $count, $above])
-      resultFile.writeln(resultOutput % [$korpus, formatFloat(accumulatedResults[korpus]/len(runs)), $above])
-      resultFile.write("\n")
+  var resultFile = open(resultFileName % [$test, $CharBased.ord, $order, $totest], fmWrite)
+  var
+    korpus = totest
+    runs: seq[TRun] = @[]
+  for line in lines("test_" & $test & "." & korpus_name):
+    if line.len > 20:
+      var run: TRun
+      # run the tests
+      run.result = recognize[TKorpus](korpi, line)
+      run.length = len(line)
+      runs.add(run)
+  # accumulate the results
+  for above in [20,50,100,150]:
+    var accumulatedResults: array[TKorpus, int]
+    for i,j in accumulatedResults: accumulatedResults[i] = 0 # clear the array
+    for run in runs:
+      if run.length < above: continue
+      inc(accumulatedResults[run.result])
+    var sum = 0
+    for guess, count in accumulatedResults:
+      sum += count
+      resultFile.writeln(guessOutput % [$korpus, $guess, $count, $above])
+    resultFile.writeln(resultOutput % [$korpus, formatFloat(accumulatedResults[korpus]/sum), $above])
+    resultFile.write("\n")
